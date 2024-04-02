0e3a115f24a1016de4656bb75f1b778879eb8495
Jesuscanwalk <beliu@s.sfusd.edu>
2024-04-02 14:01:28 -0700

    L1 and L2 the extra changes are from me opening them to record for the M4 video

---
+++ 05.loot_it_all_workbook/health_pack.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+func _ready():
+	area_entered.connect(_on_area_entered)
+
+
+func _process(delta):
+	pass
+
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
