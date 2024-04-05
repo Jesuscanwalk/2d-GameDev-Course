817e252ad52721db652e51b737150bfcc567cfcc
Jesuscanwalk <beliu@s.sfusd.edu>
2024-04-05 15:29:39 -0700

    L6

--- 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
+++ 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
@@ -16,6 +16,12 @@ func _ready() -> void:
 	set_process(false)
 
 
+func set_coin_count(new_coin_count: int) -> void:
+	coins = new_coin_count
+	get_node("UI/CoinsCount").text = "x" + str(coins)
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
 func set_target_position(new_target_position: Vector2) -> void:
 	target_position = new_target_position
 	set_process(true)
@@ -36,5 +42,8 @@ func _process(delta: float) -> void:
 
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	energy += 20.0
-	get_node("UI/EnergyBar").value = energy
+	if area_that_entered.is_in_group("coin"):
+		set_coin_count(coins + 1)
+	elif area_that_entered.is_in_group("energy"):
+		set_energy(energy + 20)
+
