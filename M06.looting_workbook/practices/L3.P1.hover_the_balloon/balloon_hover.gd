2d54ab4c113fd2ffd700073c34543323a5bb09b7
Jesuscanwalk <beliu@s.sfusd.edu>
2024-04-29 14:16:04 -0700

    L5

--- M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
+++ M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
@@ -5,9 +5,10 @@ extends Area2D
 
 
 func _ready() -> void:
+	set_outline_thickness(5.0)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	set_outline_thickness(5.0)
+	
 
 
 func set_outline_thickness(new_thickness: float) -> void:
@@ -16,9 +17,10 @@ func set_outline_thickness(new_thickness: float) -> void:
 
 
 func _on_mouse_entered() -> void:
+	set_outline_thickness(10.0)
 	var tween := create_tween()
-	tween.tween_method(set_outline_thickness, 10.0, 5.0, 5.0)
-
+	tween.tween_method(set_outline_thickness, 5.0, 10.0, 0.08)
 func _on_mouse_exited() -> void:
+	set_outline_thickness(5.0)
 	var tween := create_tween()
-	tween.tween_method(set_outline_thickness, 10.0, 5.0, 5.0)
+	tween.tween_method(set_outline_thickness, 10.0, 5.0, 0.08)
